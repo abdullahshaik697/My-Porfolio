6842ea61554569da52173d0d1e49137800292688
Abdullah Shaikh <160311441+abdullahshaik697@users.noreply.github.com>
2024-10-30 16:14:36 +0500

    Add files via upload

--- app.js
+++ app.js
@@ -34,3 +34,17 @@ valueDisplays.forEach((valueDisplays) =>{
     }
 ,duration);
 })
+
+// Navbar
+let menuList = document.getElementById("nav-menu")
+menuList.style.maxHeight = "0px"
+function toggleMenu(){
+    if(menuList.style.maxHeight == "0px"){
+
+        menuList.style.maxHeight = "fit-content"
+    }
+    else{
+        menuList.style.maxHeight = "0px"
+
+    }
+}
